5c69df1c4d61db96bf7d3ff8a36db3dcfd7a5763
Kjetil Kristoffer Solberg <kjetil.kristoffer.solberg@outlook.com>
2020-11-16 12:03:14 +0100

    Updated timeserver url for digital signing

--- DeveloperExtensions.ContextMenu/Setup-Sign.bat
+++ DeveloperExtensions.ContextMenu/Setup-Sign.bat
@@ -1,2 +1,2 @@
 ECHO CREATING DIGITAL SIGNATURE
-"C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x64\signtool" sign /sm /sha1 9392c2d39109a30b6e2138171b70698f0d7ea4b6 /t "http://timestamp.verisign.com/scripts/timstamp.dll" .\..\x64\Release\DeveloperExtensions.ContextMenu.dll
+"C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x64\signtool" sign /sm /sha1 9392c2d39109a30b6e2138171b70698f0d7ea4b6 /t "http://timestamp.comodoca.com/authenticode" .\..\x64\Release\DeveloperExtensions.ContextMenu.dll
